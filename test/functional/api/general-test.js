752c04e493e6ff09e702c5f21f01ef2bb5f920bd
YIYUAN GAO <20086441@mail.wit.ie>
2019-10-27 17:27:46 +0800

    general-test search products test finish

--- test/functional/api/general-test.js
+++ test/functional/api/general-test.js
@@ -97,5 +97,58 @@ describe("General", () => {
                 });
         }).timeout(5000);
     });
+    describe("POST /general/search", () => {
+        describe("when the keyword is valid", () => {
+            it("should GET all matching products", done => {
+                const keyword = {keyword:"rose"};
+                request(server)
+                    .post("/general/search")
+                    .send(keyword)
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        try {
+                            expect(res.body.data).to.be.a("array");
+                            expect(res.body.data.length).to.equal(2);
+                            let result = _.map(res.body.data, product => {
+                                return {
+                                    productId :product.productId,
+                                    productName:product.productName,
+                                    salePrice:product.salePrice,
+                                    productNum:product.productNum,
+                                    productImage:product.productImage,
+                                    checked:product.checked
+                                };
+                            });
+                            expect(result).to.deep.include({productId:"11",productName:"rose",salePrice:12,productNum:1,productImage:"image/rose",checked:"1"});
+                            expect(result).to.deep.include({productId:"12",productName:"rosemary",salePrice:18,productNum:12,productImage:"image/rosemary",checked:"1"});
+                            done();
+                        } catch (e) {
+                            done(e);
+                        }
+                    });
+            }).timeout(5000);
+        });
+        describe("when the keyword is valid", () => {
+            it("should return no result", done => {
+                const keyword = {keyword:"hello"};
+                request(server)
+                    .post("/general/search")
+                    .send(keyword)
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err,resp) => {
+                        expect(resp.body).to.include({
+                            message: 'no result'
+                        });
+                        done(err);
+                    });
+            }).timeout(5000);
+
+        });
+
+    });
 
 });
